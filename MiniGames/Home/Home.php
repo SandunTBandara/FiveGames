ff13f951fb9c0e005e91fa664507a5106b1af643
Sandun <94672855+SandunTBandara@users.noreply.github.com>
2023-07-15 23:35:57 -0700

    Add files via upload

---
+++ MiniGames/Home/Home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mini Games</title>
+    <link rel="stylesheet" href="Home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="./home.js" defer></script>
+</head>
+<body>
+    <div class="main-container" id="main">
+        <div class="sound-icon">
+            <img src="./sound.png" alt="" id="sound">
+            <img src="./mute.png" alt="" id="mute">
+        </div>
+        <div class="game-name" id="game-name">Mini Games! select your game</div>
+        <div class="games">
+            <div class="game">
+                <img src="./queen.png" alt="" game class="game-img" id="8 Queen Puzzle game">
+            </div>
+            <div class="game">
+                <img src="./encode.png" alt="" game class="game-img" id="Encode-Decode">
+            </div>
+            <div class="game">
+                <img src="./tictactoe.png" alt="" game class="game-img" id="Tic Tac Toe">
+            </div>
+            <div class="game">
+                <img src="./shortpath.png" alt="" game class="game-img" id="Find Sortest Path">
+            </div>
+            <div class="game" >
+                <img src="./minimum.png" alt="" game class="game-img" id="Minimum Connectors">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
